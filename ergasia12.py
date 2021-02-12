f0d4e7286a94cfb30a22c25d3708321952b385d3
p20064 <77737836+p20064@users.noreply.github.com>
2021-02-12 13:58:25 +0200

    Add files via upload

--- ergasia12.py
+++ ergasia12.py
@@ -1,26 +1,47 @@
+#########################################################
+# Η εφαρμογη ζηταει απο το χρήστη ενα αρχειο ASCII
+# Μετατρεπει τον ASII Code για καθε χαρακτηρα
+# στο συμπληρωμα του ως προς 128 (τον κατοπτρικο του)
+# Αντιστρεφει το κειμενο και εμφανιζει τους κατοπτρικους
+# χαρακτηρες με αναποδη σειρα
+#########################################################
+def get_file_content(fname):
+# Διαβαζει τα περιεχομενα του αρχειου fname
+    try:
+        with open(fname, "r", encoding = "utf-8") as f:
+            text = f.read()
+        return text
+
+    except FileNotFoundError:
+        print('Το αρχείο {} δεν βρέθηκε'.format(fname))
+        return -1
+
 def clear_keimeno(keimeno):
+    # Αφαιρει ολους τους χαρακτηρες με ASCII code
+    # που δεν ειναι αναμεσα στο 0 και το 255
     new_text=""
     for letter in keimeno:
-        if (0<=ord(letter)<=128):
+        if (0<=ord(letter)<=255):
             new_text += letter
+    return new_text
+
+def katoptriko(keimeno):
+    # Μετατρεπει καθε ASCII code στον κατοπτρικο του
+    new_text=""
+    for letter in keimeno:
+        new_text+=chr(128-ord(letter))
     return new_text
 
 filename=input("Δώσε όνομα αρχείου: ")
 if filename=='': exit
 
-try:
-    with open(filename, "r", encoding = "utf-8") as f:
-        text = f.read()
+text=get_file_content(filename)
 
+if (text!=-1):
     text=clear_keimeno(text)
+    # Αντιστροφη του κειμενου
     text=text[::-1]
+    # Μετατροπη καθε χαρακτηρα στον κατοπτρικο
+    text=katoptriko(text)
 
-    new_text=''
-    for letter in text:
-        new_text+=chr(128-ord(letter))
-
-    print(new_text + "\n")
-
-except FileNotFoundError:
-    print('Το αρχείο {} δεν βρέθηκε'.format(filename))
-    exit
+    print(text + "\n")
